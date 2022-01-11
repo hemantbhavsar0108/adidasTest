240bf5563d275338434575b22e845a7def7f9de8
hemantbhavsar0108 <94780630+hemantbhavsar0108@users.noreply.github.com>
2022-01-11 16:14:27 +0200

    committing API project for adidas

---
+++ com.adidas.test.cucumber.api/bin/target/cucumber-reports/report.js
@@ -0,0 +1,227 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/java/com/adidas/test/com/adidas/test/cucumber/api/cucumber/features/petStoreAPIMethodExecution.feature");
+formatter.feature({
+  "name": "Validate pet store functionaility",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "name": "Get  pet details based on status",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "Access get pet store method",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.adidas.test.com.adidas.test.cucumber.api.cucumber.stepdefinition.StepDefinition.access_get_pet_store_method()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Get pet information based on status \"available\" method  executed",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "com.adidas.test.com.adidas.test.cucumber.api.cucumber.stepdefinition.StepDefinition.get_pet_information_based_on_status_method_executed(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Validate response status code for get pet status method execution",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.adidas.test.com.adidas.test.cucumber.api.cucumber.stepdefinition.StepDefinition.validate_response_status_code_for_get_pet_status_method_execution()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Validate the reponse body for get pet status method execution",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.adidas.test.com.adidas.test.cucumber.api.cucumber.stepdefinition.StepDefinition.validate_the_reponse_body_for_get_pet_status_method_execution()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Validate the response header for get pet status method execution",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.adidas.test.com.adidas.test.cucumber.api.cucumber.stepdefinition.StepDefinition.validate_the_response_header_for_get_pet_status_method_execution()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Create new pet",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "Access create pet store method",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.adidas.test.com.adidas.test.cucumber.api.cucumber.stepdefinition.StepDefinition.access_create_pet_store_method()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Create new pet method  executed with id \"121\" and name \"name_pet_121\" and status \"available\"",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "com.adidas.test.com.adidas.test.cucumber.api.cucumber.stepdefinition.StepDefinition.create_new_pet_method_executed_with_id_and_name_and_status_and_photourl_and_tag(java.lang.String,java.lang.String,java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Validate response status code for Create new pet execution",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.adidas.test.com.adidas.test.cucumber.api.cucumber.stepdefinition.StepDefinition.validate_response_status_code_for_Create_new_pet_execution()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Validate the reponse body for Create new pet execution",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.adidas.test.com.adidas.test.cucumber.api.cucumber.stepdefinition.StepDefinition.validate_the_reponse_body_for_Create_new_pet_execution()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Validate the response header for Create new pet method execution",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.adidas.test.com.adidas.test.cucumber.api.cucumber.stepdefinition.StepDefinition.validate_the_response_header_for_Create_new_pet_method_execution()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Update new pet",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "Access Update pet store method",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.adidas.test.com.adidas.test.cucumber.api.cucumber.stepdefinition.StepDefinition.access_Update_pet_store_method()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Update new pet method  executed with id \"121\" and name \"name_pet_121\" and field \"status\" with \"sold\"",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "com.adidas.test.com.adidas.test.cucumber.api.cucumber.stepdefinition.StepDefinition.update_new_pet_method_executed_with_id_and_name_and_field_with(java.lang.String,java.lang.String,java.lang.String,java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Validate response status code \"200\" for update new pet execution",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.adidas.test.com.adidas.test.cucumber.api.cucumber.stepdefinition.StepDefinition.validate_response_status_code_for_update_new_pet_execution(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Validate the reponse body with id \"121\" for update new pet execution",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.adidas.test.com.adidas.test.cucumber.api.cucumber.stepdefinition.StepDefinition.validate_the_reponse_body_with_id_for_update_new_pet_execution(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Validate the response header for update new pet method execution",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.adidas.test.com.adidas.test.cucumber.api.cucumber.stepdefinition.StepDefinition.validate_the_response_header_for_update_new_pet_method_execution()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.scenario({
+  "name": "Delete pet information",
+  "description": "",
+  "keyword": "Scenario"
+});
+formatter.step({
+  "name": "Access delete pet store method",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "com.adidas.test.com.adidas.test.cucumber.api.cucumber.stepdefinition.StepDefinition.access_delete_pet_store_method()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Delete pet method  executed with id \"121\"",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "com.adidas.test.com.adidas.test.cucumber.api.cucumber.stepdefinition.StepDefinition.delete_pet_method_executed_with_id(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Validate response status code \"200\" for delete pet execution",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.adidas.test.com.adidas.test.cucumber.api.cucumber.stepdefinition.StepDefinition.validate_response_status_code_for_delete_pet_execution(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Validate the reponse body with message \"deleted\" for delete pet execution",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.adidas.test.com.adidas.test.cucumber.api.cucumber.stepdefinition.StepDefinition.validate_the_reponse_body_with_message_for_delete_pet_execution(java.lang.String)"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Validate the response header for delete pet method execution",
+  "keyword": "Then "
+});
+formatter.match({
+  "location": "com.adidas.test.com.adidas.test.cucumber.api.cucumber.stepdefinition.StepDefinition.validate_the_response_header_for_delete_pet_method_execution()"
+});
+formatter.result({
+  "status": "passed"
+});
+});
